127628be32467f5236a04e2450a4137d5c400119
elivious <xamrucs74@gmail.com>
2017-06-08 22:37:02 -0700

    Commit Index

--- core/main.js
+++ core/main.js
@@ -1,2 +1,38 @@
-define([
-], function(){});
+require.config({
+    baseUrl: "/Photo-Host",
+    paths: {
+        "some": "some/v1.0"
+    },
+    waitSeconds: 15
+});
+
+requirejs([
+    "scripts/text",
+    "core/router",
+    "text!pages/Search-Results.html",
+    "text!pages/User-Profile.html",
+    "text!pages/User-Images.html"
+], function(text, Router, Search, profile, Images){
+
+    Router.on('router-event', function(path){
+        console.log("path", path);
+        switch(path){
+            case 'start':
+                $('.content').html(text);
+                break;
+            case 'Router':
+                $('.content').html(Router);
+                break;
+            case 'search':
+                $('.content').html(Search);
+                break;
+            case 'profile':
+                $('.content').html(profile);
+                break;
+            case 'Images':
+                $('.content').html(Images);
+                break;
+        }
+
+    });
+});
--- core/router.js
+++ core/router.js
@@ -1,3 +1,50 @@
-define([],
-    function () {
+define([
+
+], function(
+
+){
+    var AppRouter = Backbone.Router.extend({
+        routes: {
+            "": 'start',
+            "Results": "Search",
+            "Profile": 'profile',
+            "My-Images": 'Images'
+        },
+
+        start: function() {
+            router.trigger('router-event', 'start')
+        },
+
+        Search: function() {
+            router.trigger('router-event', 'search')
+        },
+        profile: function() {
+            router.trigger('router-event', 'profile')
+        },
+        Images: function() {
+            router.trigger('router-event', 'Images')
+        }
     });
+
+    router = new AppRouter();
+
+    $("body").on("click","a:not(a[data-bypass])",function(e){
+        // block the default link behavior
+        e.preventDefault();
+        if($(e.currentTarget).attr('nav') != "false"){
+
+            // take the href of the link clicked
+            var href = $(this).attr("href");
+
+            // pass this link to Backbone
+            Backbone.history.navigate(href, true);
+
+            router.navigate(href, true);
+        }
+    });
+
+    Backbone.history.start({ pushState: true });
+
+    return router;
+
+})
